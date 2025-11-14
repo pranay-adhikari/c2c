1b4834e6348f9044e50d3086e1c94ec572ccc490
Pranay <pranay.adhi@gmail.com>
2025-11-13 19:12:12 -0600

    finishing touches

--- Delivery App Chatbot/deliveryappchatbot.py
+++ Delivery App Chatbot/deliveryappchatbot.py
@@ -3,14 +3,11 @@ import re
 import random
 from order import Order
 
-def welcome_user():
-    print("\nHi there! Sorry your delivery hasn't arrived as expected; let's get this sorted.")
-
 def display_menu():
     while True:
         print('-----------------------------------------')
         user_choice = input(f'Please choose from the following options:\n'
-        '1) Estimated time of arrival\n2) Request delay reason\n3) Resolution options\n4) Connect with human support\n5) Exit the conversation\n\n')
+        '1) Estimated time of arrival\n2) Request delay reason\n3) Resolution options\n4) Connect with human support\n5) Exit the conversation\n\n').strip()
         print()
         if re.match('^[1-5]$', user_choice):
             return int(user_choice)
@@ -20,6 +17,9 @@ def display_menu():
 def handle_user_choice(user_choice, account):
     order = account["current_order"]
     is_premium = account["premium"]
+    name = account["name"]
+    age = account["age"]
+
     match user_choice:
         case 1:
             print(Order.get_ETA(order))
@@ -29,8 +29,9 @@ def handle_user_choice(user_choice, account):
             print(Order.get_resolution_options(order, is_premium))
             print('We hope you are satisfied with the outcome!')
         case 4:
-            print(Order.human_support(order))
+            print(Order.human_support(order, age, name))
         case 5:
+            print(f'Thank you for using the delivery support chatbot. Goodbye!')
             return False
     return True
 
@@ -40,13 +41,24 @@ def menu_loop(account):
         user_choice = display_menu()
         running = handle_user_choice(user_choice, account)
 
+# Creates the user account with a chance for premium
 def simulate_customer_account():
+    print("\nHi there! Sorry your delivery hasn't arrived as expected; let's get this sorted.")
+    name = input('What is your name?: ').strip()
+    while True:
+        age = input('What is your age?: ').strip()
+        if age.isdigit():
+            age = int(age)
+            break
+        else:
+            print('Not a valid age')
+
+    print(f'\nThanks {name}! I\'ll help you right away.')
     subscribed_to_premium = random.randint(0, 1) == 1
     order = Order()
-    return {"current_order":order, "premium":subscribed_to_premium}
+    return {"current_order":order, "premium":subscribed_to_premium, "name":name, "age":age}
 
 def main():
-    welcome_user()
     menu_loop(simulate_customer_account())
 
 if __name__ == '__main__':
--- Delivery App Chatbot/order.py
+++ Delivery App Chatbot/order.py
@@ -1,4 +1,5 @@
 import random
+import time
 class Order:
     def __init__(self):
         self.status = random.randint(1, 10)
@@ -8,9 +9,11 @@ class Order:
         self.phone_number = f'{random.randint(100, 999)}-{random.randint(1000, 9999)}-{random.randint(1000, 9999)}'
         self.is_resolved = False
 
+    # Returns estimated time of arrival message
     def get_ETA(self):
         return f"Your estimated time of arrival is {self.ETA}{' minutes' if self.ETA != 'NA' else ''}\n"
 
+    # Returns delay reason message based on a randomized status
     def get_delay_reason(self):
         match self.status:
             case 1:
@@ -20,13 +23,15 @@ class Order:
             case 3:
                 return 'Your driver is driving to you\n'
             case 4:
+                time.sleep(1)
                 self.status = random.randint(1, 10)
-                return 'Unable to determine a delay reason; please try again shortly\n'
+                return 'Unable to determine a delay reason; please try again\n'
             
             case _:
                 self.ETA = "NA"
                 return 'Your driver is not able to make it. We can offer a full refund or equivalent credits in the \'Resolution options\' tab\n'
     
+    # Handles resolution for messed up orders. Premium accounts get 25% credit increase
     def get_resolution_options(self, is_premium):
         new_balance = (self.price * 100 + self.credits) * 1.25 if is_premium else self.price * 100 + self.credits
         to_be_added = new_balance - self.credits
@@ -52,5 +57,8 @@ class Order:
         else:
             return 'Your order has been reimbursed' if self.is_resolved else 'You currently do not qualify for resolution options'
     
-    def human_support(self):
-        return f'Please call \'{self.phone_number}\' to address any concerns over your order'
+    # Returns personalized human support message. If age < 13, the user is told their guardian must call the support number
+    def human_support(self, age, name):
+        print(f'Connecting you to a human agent, {name}. Please hold...')
+        time.sleep(1)
+        return f'Please call \'{self.phone_number}\' to address any concerns over your order' if age >= 13 else f'Please tell a guardian to call \'{self.phone_number}\' to address any concerns over your order'
